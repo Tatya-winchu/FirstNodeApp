1d4f9a58dc30560f3e308b2a7affcbd2827f5eae
kedardahivale <kedar.dahiwale@gmail.com>
2019-03-07 22:38:05 +0100

    definition of class

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//definition of class 
+class Rectangle {
+    constructor(){
+        this.x = 20;
+        this.y =10;
+        console.log('Creating a rectangle');
+    }
+}
+//creating a instance 
+let Rect1 = new Rectangle();
+console.log(Rect1)
