0c78be32bd0cc0619fef5706612b01c73a36e879
kedardahivale <kedar.dahiwale@gmail.com>
2019-03-07 23:07:10 +0100

    beautification of app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 //creating a instance 
 let class1 = require('./class.js')
 let Rect1 = new class1(2,3);
-console.log(Rect1)
-console.log(Rect1.getArea());
+console.log(`Area of rectangle: ${Rect1.getArea()}`);
+Rect1.printDescription();
 // New Object
 let Rect2 = new class1(4,8);
-console.log(Rect2)
-console.log(Rect2.getArea());
+console.log(`Area of rectangle: ${Rect2.getArea()}`);
+Rect2.printDescription();
--- class.js
+++ class.js
@@ -1,13 +1,18 @@
 //definition of class 
 class Rectangle {
     constructor(_height,_width){
+        console.log('The rectangle is being created');
         this.height = _height;
         this.width =_width;
-        console.log(`Creating a rectangle ${this.width}*${this.height}`);
+        
     }
     //adding a getter 
      getArea(){
         return this.width*this.height
+    }
+    //description method
+    printDescription(){
+        console.log(`Creating a rectangle of ${this.width}*${this.height}`);
     }
 }
 
